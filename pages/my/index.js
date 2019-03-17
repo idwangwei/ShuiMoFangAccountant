a981e070c6f1be5eb914d2e2a8baecc250fb4042
idwangwei <idwangwei@126.com>
2019-03-17 21:05:46 +0800

    进度更新

--- pages/my/index.js
+++ pages/my/index.js
@@ -103,40 +103,4 @@ Page({
     },
 
 
-
-    reLogin:async function() {  // 确保有用户信息
-        await new Promise(resolve => { // ⚠️注意开头有await！！！
-            wx.getSetting({
-                success: (res) => {        // 如果用户没有授权或者没有必要的用户信息
-                    if (!res.authSetting['scope.userInfo'] || !_.isRealTrue(wx.getStorageSync('userInfoRes').userInfo)) {
-                        navToLogin(resolve) // 去提示用户点击登录按钮，⚠️注意：并把当前的resolve带过去
-                    } else {
-                        resolve() // 静默登录
-                    }
-                }
-            })
-        });
-        return new Promise((resolve) => {
-            wx.login({
-                success: res => {
-                    login(res.code).then((jwt) => {
-                        resolve(jwt) // resolve jwt
-                    }) // 通过code进行登录
-                },
-                fail(err) {
-                    wx.showToast({
-                        title: err.errMsg,
-                        icon: 'none',
-                        duration: 2000
-                    })
-                }
-            })
-        })
-    },
-    navToLogin:function (resolve){  /* eslint-disable no-undef */
-        const pages = getCurrentPages();
-        const page = pages[pages.length - 1];// 当前page
-        page.openLoginModal(resolve)// 打开登录按钮弹框，并把当前的resolve带过去
-    },
-
 });
